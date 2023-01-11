5587a36c4033e27ca16458935b1116bab9ef141b
Guillaume Crégut <gcregut@free.fr>
2023-01-11 10:51:39 +0100

    refactoring js and HTML for DRY with price

--- food.js
+++ food.js
@@ -1,17 +1,23 @@
-//Changing like for grills
+//récupération dans le dom du div "like"
 const grillsLike=document.getElementById('grills-like');
 grillsLike.addEventListener('click',()=>{
-    grillsLike.classList.toggle('like')
-});
-
-//Changing price for grills
-const grillPrice=20;
-const grillQuantitySelector=document.getElementById('grills-qty');
-
-grillQuantitySelector.addEventListener('change',()=>{
-    const grillQty=grillQuantitySelector.value;
-    const totalGrillPrice=grillPrice*grillQty;
-    const displayGrillTotalPrice=document.getElementById('grills-price');
-    displayGrillTotalPrice.innerHTML=totalGrillPrice;
+    grillsLike.classList.toggle('like');
 })
 
+//function de calcul
+const calculPrice=(price,qtty,element)=>{
+    let total=price*qtty;
+    const spanPrice=document.getElementById(element);
+    spanPrice.innerHTML=total;
+}
+//Assigne les eventListener à chaque menu déroulant
+//Dans le fichier HTML, j'ai rajouter des data-span et data-price dans les sélecteurs
+const selectors=document.querySelectorAll('select');  //on récupère tous les select du document
+for(const selector of selectors){
+    selector.addEventListener('change',()=>{
+        const price=selector.dataset.price;
+        const element=selector.dataset.span;
+        const qtty=selector.value;
+        calculPrice(price,qtty,element);
+    });
+}
